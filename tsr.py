d8d638897488e6aa4e5c54e2b777e44658f0e0f2
George <gc14768@my.bristol.ac.uk>
2017-12-13 12:08:55 +0000

    Creating new batch_generator object each time

--- tsr.py
+++ tsr.py
@@ -190,14 +190,11 @@ def main(_):
             #(trainImages, trainLabels) = cifar.getTrainBatch()
             #(testImages, testLabels) = cifar.getTestBatch()
 
-            #(trainImages, trainLabels) = trainGenerator.next()
-            #(testImages, testLabels) = testGenerator.next()
-            for (trainImages, trainLabels) in bg.batch_generator(data, 'train'):
+            (trainImages, trainLabels) = bg.batch_generator(data,'train').next()
+            (testImages, testLabels) = bg.batch_generator(data,'test').next()
 
-                _, train_summary_str = sess.run([train_step, train_summary],
+            _, train_summary_str = sess.run([train_step, train_summary],
                                       feed_dict={x_image: trainImages, y_: trainLabels})
-
-            (testImages, testLabels) = bg.batch_generator(data, 'test').next()
 
             # Validation: Monitoring accuracy using validation set
             if (step + 1) % FLAGS.log_frequency == 0:

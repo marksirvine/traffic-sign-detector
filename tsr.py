31573396060cc1fde876bdd2d6a308574f8ed832
George <gc14768@my.bristol.ac.uk>
2017-12-16 16:01:57 +0000

    Set tensorflow batch-size constant to 100

--- tsr.py
+++ tsr.py
@@ -49,7 +49,7 @@ tf.app.flags.DEFINE_string('log-dir', '{cwd}/logs/'.format(cwd=os.getcwd()),
 # Optimisation hyperparameters
 tf.app.flags.DEFINE_integer('max-steps', 10000,
                             'Number of mini-batches to train on. (default: %(default)d)')
-tf.app.flags.DEFINE_integer('batch-size', 128, 'Number of examples per mini-batch. (default: %(default)d)')
+tf.app.flags.DEFINE_integer('batch-size', 100, 'Number of examples per mini-batch. (default: %(default)d)')
 tf.app.flags.DEFINE_float('learning-rate', 1e-3, 'Number of examples to run. (default: %(default)d)')
 
 IMG_WIDTH = 32

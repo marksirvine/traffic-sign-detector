e5c8d5829fbf4a325e713fd828c3146704749a5a
George <gc14768@my.bristol.ac.uk>
2018-01-11 19:56:40 +0000

    Revert "Created function to normalize an image"
    This reverts commit 00ba3d2396c0f1a6ad8fb91f244ad801750d2015.

--- tsr.py
+++ tsr.py
@@ -24,8 +24,6 @@ import cPickle as pickle
 
 import scipy as scp
 
-import numpy as np
-
 data = pickle.load(open('dataset.pkl','rb'))
 
 here = os.path.dirname(__file__)
@@ -222,13 +220,14 @@ def main(_):
 
         sess.run(tf.global_variables_initializer())
 
-        #A = [[[-1.5,2.5], [-1.5,2.5]],
-        #    [[1.5,2.5], [1.5,2.5]],
-        #    [[1.5,2.5], [1.5,2.5]]]
+        #A = [[[1,2,9],[2,2,8],[3,2,7]],
+        #     [[4,2,6],[5,2,5],[6,2,4]],
+        #     [[7,2,3],[8,2,2],[9,2,1]]]
 
+        #image = tf.placeholder(tf.float32)
 
-        #print(A)
-        #print(normalizeImage(A))
+        #new_image = normalization(image)
+        #print(sess.run(new_image, feed_dict={image: A}))
 
 
         #variable to store the previous validation accuracy
@@ -347,28 +346,29 @@ def weightDecay(sess, oldWeights):
        newWeights = getCurrentWeights()[i] - FLAGS.learning_rate * FLAGS.weight_decay * oldWeights[i]
        sess.run(tf.assign(tf.get_collection_ref(tf.GraphKeys.TRAINABLE_VARIABLES)[i], newWeights))
 
-def normalizeImage(image):
-    newImage = np.array(image)
-    print(newImage.shape)
+def normalization(image):
 
-    #sum up the channels
-    mean = [0,0,0];
-    for i in range(newImage.shape[0]):
-        for j in range(newImage.shape[1]):
-            for k in range(newImage.shape[2]):
-                mean[k] += image[i][j][k]
+    #get each channel
+    rChannel = image[:,:,0]
+    gChannel = image[:,:,1]
+    bChannel = image[:,:,2]
 
-    #divide sum by the total number of pixels
-    for i in range(newImage.shape[2]):
-        mean[i] /= (newImage.shape[0] * newImage.shape[1])
+    #calculate the mean for each channel
+    rMean = tf.reduce_mean(rChannel)
+    gMean = tf.reduce_mean(gChannel)
+    bMean = tf.reduce_mean(bChannel)
 
-    #subtract the mean from the image copy
-    for i in range(newImage.shape[0]):
-        for j in range(newImage.shape[1]):
-            for k in range(newImage.shape[2]):
-                newImage[i][j][k] -= mean[k]
+    rNormalized = rChannel - rMean
+    gNormalized = gChannel - gMean
+    bNormalized = bChannel - bMean
 
-    return newImage
+    rNormalized = tf.stack([rNormalized],2)
+    gNormalized = tf.stack([gNormalized],2)
+    bNormalized = tf.stack([bNormalized],2)
+
+    new_image = tf.concat([rNormalized, gNormalized, bNormalized],2)
+
+    return new_image
 
 
 def imageWhitening(image):
